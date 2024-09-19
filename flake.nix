fc4d0ea31018d4e4104667385e59570fab9e11e0
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-19 02:11:14 +0200

    use papermod/master

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "flake-utils";
+    papermod = {
+      url = "github:adityatelange/hugo-papermod";
+      flake = false;
+    };
   };
 
   outputs =
@@ -9,17 +13,12 @@
       self,
       nixpkgs,
       flake-utils,
+      papermod,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        papermod = pkgs.fetchFromGitHub {
-          owner = "adityatelange";
-          repo = "hugo-PaperMod";
-          rev = "v7.0";
-          hash = "sha256-33EnCEvTxZYn31fxZkYJlQXvJsczXMVufSj6QJJHrLk=";
-        };
         website = pkgs.stdenv.mkDerivation {
           name = "website";
           src = ./.;
